342ea4c302b7b621d0adbd6a45dbf73c9d696f17
Egsagno <egsagon12@gmail.com>
2022-09-14 18:23:25 +0200

    [s2] end dm + mv tmp ex

--- recursion/dm1.py
+++ recursion/dm1.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
 
-# === Ex7 ===
+# === Ex7 === #
 
-def li_cat(n):
-    if n == 0: return [1]
+PAR = ('(', ')')
+
+def cat(n: int) -> list:
+    if not n: return [1]
     
     n -= 1
-    l = li_cat(n)
+    li = cat(n)
+    return li + [sum([li[i] * li[n - i] for i in range(n + 1)])]
+
+# print(cat(7))
+
+def par(n: int, li = []) -> list:
+    if not n: return ['']
+    if n == 1: return [''.join(PAR)]
     
-    return l + [sum([l[i] * l[n - i] for i in range(n + 1)])]
+    return [f'{PAR[0]}{b}{PAR[1]}{a}'
+            for i in range(n)
+                for b in par(i)
+                    for a in par(n - 1 - i)]
 
-# print(li_cat(7))
-# TODO: include l as an argument
+# print(par(3))
 
-# === Ex9 ===
+# === Ex9 === #
 
-def recherche_dico(l: list, x: int) -> bool:
-    if not len(l): return False
+def rec(l: list, x: int) -> bool:
+    if not len(l) or x < l[0] or x > l[-1]: return False
     if x == l[0]: return True
     
-    med = len(l) // 2
+    m = len(l) // 2
     
-    if x == l[med]: return True
-    elif x < l[med]: return recherche_dico(l[:med], x)
-    elif x > l[med]: return recherche_dico(l[med:], x)
+    if x == l[m]: return True
+    elif x < l[m]: return rec(l[:m], x)
+    elif x > l[m]: return rec(l[m:], x)
 
-# print(recherche_dico([0, 1, 2, 4, 18], 2))
+# print(rec(list(range(100)), 10))
+# print(rec([1, 2, 3], 4))
